a9d737fecb0e8b7e5735fa1e3903af974b10ef40
Pawel Switalski <pawelswitalski94@gmail.com>
2021-04-04 16:38:19 +0200

    Create Navigation and Button components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import '../styles/App.scss';
+import Navigation from './Navigation/Navigation';
 
 class App extends Component {
   render() {
     return(
       <>
-        <nav></nav>
+        <nav>
+          <Navigation/>
+        </nav>
         <header></header>
         <main></main>
         <footer></footer>
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button/Button';
+
+const buttons = ["start", "portfolio", "o mnie", "kontakt"];
+
+const Navigation = () => {
+    return(
+        <>
+            <Button/>
+        </>
+    )
+}
+
+export default Navigation;
