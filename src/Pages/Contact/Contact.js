b4d6f96174aed3fda96da28df08b530d3365aa5b
Pawel Switalski <pawelswitalski94@gmail.com>
2021-04-13 12:10:13 +0200

    create Form file

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Form from '../../components/Form/Form';
 
 const Contact = () => {
     return(
-        <main>Kontakt</main>
+        <main>
+            <Form/>
+        </main>
     )
 }
 
