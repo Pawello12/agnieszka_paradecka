9bf2de054cb162cb42b1973eece659db4042c6cb
Pawel Switalski <pawelswitalski94@gmail.com>
2021-04-13 12:44:30 +0200

    create form structure and basic css

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import {useFormik} from 'formik';
+import './Form.scss';
 
 const Form = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: ''
+        }
+    })
+
+
+
     return(
-        <div>formularz</div>
+        <form className="form">
+            <label className="form__label" htmlFor="name">Imię i nazwisko:</label>
+            <input className="form__input" type="text" id="name"/>
+            <label className="form__label" htmlFor="email">E-mail:</label>
+            <input className="form__input" type="email" id="email"/>
+            <label className="form__label" htmlFor="message">Treść wiadomości:</label>
+            <textarea className="form__textarea" name="message" id="message"></textarea>
+            <button className="form__button">Wyślij wiadomość</button>
+        </form>
     )
 }
 
