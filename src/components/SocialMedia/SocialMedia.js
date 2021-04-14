b4a374f656f79f12aeb343b4d505b1d011108a11
Pawel Switalski <pawelswitalski94@gmail.com>
2021-04-14 11:59:56 +0200

    install fontawesome package and create socialmedia links for mobile

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import SocialMediaLink from './SocialMediaLink/SocialMediaLink';
 import './SocialMedia.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+const facebookIcon = <FontAwesomeIcon icon={faFacebook} />;
+const instagramIcon = <FontAwesomeIcon icon={faInstagram} />
+const mailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+
+const facebookUrl = "https://www.facebook.com/aparadecka";
+const instagramUrl = "https://www.instagram.com/kitkacy";
+const mailToUrl = "mailto:kontaktkitkacy@gmail.com";
 
 const SocialMedia = () => {
     return(
         <div className="social-media">
-            <SocialMediaLink />
-            <SocialMediaLink />
-            <SocialMediaLink />
+            <SocialMediaLink name="facebook" icon={facebookIcon} type="link" url={facebookUrl} />
+            <SocialMediaLink name="instagram" icon={instagramIcon} type="link" url={instagramUrl} />
+            <SocialMediaLink mail1="aparadecka93" mail2="@gmail.com" icon={mailIcon} type="mail" url={mailToUrl} />
         </div>
     )
 }
--- src/components/SocialMedia/SocialMediaLink/SocialMediaLink.js
+++ src/components/SocialMedia/SocialMediaLink/SocialMediaLink.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-const SocialMediaLink = () => {
+const SocialMediaLink = ({name, icon, url, type, mail1, mail2}) => {
+
+    const typeCheck = () => {
+        if (type === 'link')  {
+            return (
+                <>
+                    <a className="social-media__a" href={url}><div className="social-media__content">{icon}</div></a>
+                    <a className="social-media__a" href={url}><div className="social-media__name"><p>{name}</p></div></a>
+                </>
+            )
+        } else if (type === 'mail') {
+            return (
+                <>
+                    <a className="social-media__a" href={url}><div className="social-media__content">{icon}</div></a>
+                    <a className="social-media__a" href={url}><div className="social-media__name social-media__name-mail"><p>{mail1}</p><p>{mail2}</p></div></a>
+                </>
+            )
+        }
+    }
+
     return(
         <div className="social-media__link">
-            <div className="social-media__content">link</div>
-            <div className="social-media__name">name</div>
+            {typeCheck()}
         </div>
 
     )
