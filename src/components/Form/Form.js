fe9b3e97960cd0af87a1a37b4cff80a01546bb9c
Pawel Switalski <pawelswitalski94@gmail.com>
2021-05-05 13:53:25 +0200

    form can now submit emails

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,21 +8,37 @@ const serviceData = {
     userID: 'user_gB3GFH3HLsV0Ah1F7AoIQ'
 }
 
+
+
 const Form = () => {
 
     const [name, changeName] = useState('');
     const [email, changeEmail] = useState('');
     const [message, changeMessage] = useState('');
 
+
+
     const submitHandler = (e) => {
         e.preventDefault();
+
+        const messages = document.querySelectorAll('.form__info-message');
+        const positiveMessage = document.querySelector('.form__info-positive');
+        const negativeMessage = document.querySelector('.form__info-negative');
+
+        messages.forEach(message => message.classList.remove('form__info-active'));
+
         console.log(name, email, message);
         emailjs.sendForm(serviceData.serviceID, serviceData.templateID, e.target, serviceData.userID)
         .then((result) => {
             console.log(result.text);
+
+            positiveMessage.classList.add('form__info-active');
         }, (error) => {
             console.log(error.text);
-        });    }
+
+            negativeMessage.classList.add('form__info-active');
+        });
+    }
 
     return(
         <form className="form" onSubmit={submitHandler}>
@@ -34,6 +50,10 @@ const Form = () => {
             <label className="form__label" htmlFor="message">Treść wiadomości:</label>
             <textarea className="form__textarea" name="message" id="message" value={message} onChange={(e) => changeMessage(e.target.value)} required></textarea>
             <button className="form__button" type="submit">Wyślij wiadomość</button>
+            <div className="form__info">
+                <p className="form__info-message form__info-positive">Twoja wiadomość została wysłana!</p>
+                <p className="form__info-message form__info-negative">Coś poszło nie tak :(</p>
+            </div>
         </form>
     )
 }
